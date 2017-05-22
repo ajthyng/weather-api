a04d9c3f96fadb40cba8e9bd040806625c700225
Andrew Thyng <andrew.thyng@gmail.com>
2017-05-21 22:50:19 -0500

    Added morgan simple logging

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path');
 const weather = require('./Weather/EndPoints.js');
 const elements = require('./Weather/Elements.js');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const ROOT = __dirname;
 const app = express();
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 app.use(bodyParser.raw({ extended: false }));
 app.use(bodyParser.json({ extended: false }));
 
